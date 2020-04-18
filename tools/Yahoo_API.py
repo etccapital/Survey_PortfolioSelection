512cdd85db3ad7ddb98d96659855a23a7f2011e7
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-17 23:44:56 -0400

    Update Yahoo_API.py

--- tools/Yahoo_API.py
+++ tools/Yahoo_API.py
@@ -11,3 +11,19 @@ end_date = '2016-12-31'
 
 # User pandas_reader.data.DataReader to load the desired data. As simple as that.
 panel_data = data.DataReader('INPX', 'google', start_date, end_date)
+
+# Getting just the adjusted closing prices. This will return a Pandas DataFrame
+# The index in this DataFrame is the major index of the panel_data.
+close = panel_data['Close']
+
+# Getting all weekdays between 01/01/2000 and 12/31/2016
+all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
+
+# How do we align the existing prices in adj_close with our new set of dates?
+# All we need to do is reindex close using all_weekdays as the new index
+close = close.reindex(all_weekdays)
+
+# Reindexing will insert missing values (NaN) for the dates that were not present
+# in the original set. To cope with this, we can fill the missing by replacing them
+# with the latest available price for each instrument.
+close = close.fillna(method='ffill')
