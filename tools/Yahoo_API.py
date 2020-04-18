f8e80a23067595d8f1271251d5c368612e172cbc
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-17 23:45:54 -0400

    Repo Maintenance

--- tools/Yahoo_API.py
+++ tools/Yahoo_API.py
@@ -5,9 +5,9 @@ import pandas as pd
 # Define the instruments to download. We would like to see Apple, Microsoft and the S&P500 index.
 tickers = ['AAPL', 'MSFT', '^GSPC']
 
-# We would like all available data from 01/01/2000 until 12/31/2016.
-start_date = '2010-01-01'
-end_date = '2016-12-31'
+# We would like all available data from 01/01/2018 until 12/31/2019.
+start_date = '2018-01-01'
+end_date = '2019-12-31'
 
 # User pandas_reader.data.DataReader to load the desired data. As simple as that.
 panel_data = data.DataReader('INPX', 'google', start_date, end_date)
@@ -16,7 +16,7 @@ panel_data = data.DataReader('INPX', 'google', start_date, end_date)
 # The index in this DataFrame is the major index of the panel_data.
 close = panel_data['Close']
 
-# Getting all weekdays between 01/01/2000 and 12/31/2016
+# Getting all weekdays between 01/01/2018 and 12/31/2019
 all_weekdays = pd.date_range(start=start_date, end=end_date, freq='B')
 
 # How do we align the existing prices in adj_close with our new set of dates?
